2434bbc10d8d0ac068a290524055e1fd9585fdcf
Cloud <Cloud@cloud101.com>
2021-10-10 16:01:11 +0800

    add splash screen control

--- index.js
+++ index.js
@@ -5,23 +5,25 @@
 import { Navigation } from "react-native-navigation";
 //import {AppRegistry} from 'react-native';
 import App from './App';
+import SplashScreen from 'react-native-splash-screen'
 //import {name as appName} from './app.json';
 
 //AppRegistry.registerComponent(appName, () => App);
 
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
-   Navigation.setRoot({
-     root: {
-       stack: {
-         children: [
-           {
-             component: {
-               name: 'com.myApp.WelcomeScreen'
-             }
-           }
-         ]
-       }
-     }
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.WelcomeScreen'
+            }
+          }
+        ]
+      }
+    }
   });
+  SplashScreen.hide();
 });
